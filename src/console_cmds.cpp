3fe4a7846eadc2899bdb71415e997a9eae8fe2ff
pelya <x.pelya.x@gmail.com>
2014-07-19 21:21:19 +0300

    Added shorter command 'cheats' to open cheats window

--- src/console_cmds.cpp
+++ src/console_cmds.cpp
@@ -1943,6 +1943,7 @@ void IConsoleStdLibRegister()
 	IConsoleCmdRegister("saveconfig",   ConSaveConfig);
 	IConsoleCmdRegister("ls",           ConListFiles);
 	IConsoleCmdRegister("open_cheats",  ConOpenCheats);
+	IConsoleCmdRegister("cheats",       ConOpenCheats);
 	IConsoleCmdRegister("cd",           ConChangeDirectory);
 	IConsoleCmdRegister("pwd",          ConPrintWorkingDirectory);
 	IConsoleCmdRegister("clear",        ConClearBuffer);
