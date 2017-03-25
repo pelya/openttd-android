f0daeb381234943bc220e85ea741869a589dba55
pelya <x.pelya.x@gmail.com>
2017-03-25 23:18:16 +0200

    Updated translation script

--- android-extract-strings.sh
+++ android-extract-strings.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p ../translations/lang
-git diff 1.6/master -- src/lang/english.txt | tail -n +5 | grep '^[+]' | cut -b 2- | \
+git diff 1.7/master -- src/lang/english.txt | tail -n +5 | grep '^[+]' | cut -b 2- | \
 grep -v "^STR_TABLET_CLOSE\b" | \
 grep -v "^STR_TABLET_SHIFT\b" | \
 grep -v "^STR_TABLET_CTRL\b" | \
