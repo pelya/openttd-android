3405519815a5754f4ead9cab8632ed53eed41755
Sergii Pylypenko <x.pelya.x@gmail.com>
2019-04-24 18:03:01 +0300

    Fixed missing enum in music widget

--- src/widgets/music_widget.h
+++ src/widgets/music_widget.h
@@ -16,8 +16,10 @@
 enum MusicTrackSelectionWidgets {
 	WID_MTS_CAPTION,    ///< Window caption.
 	WID_MTS_LIST_LEFT,  ///< Left button.
+	WID_MTS_LEFT_SCROLLBAR,  ///< Scrollbar of left list.
 	WID_MTS_PLAYLIST,   ///< Playlist.
 	WID_MTS_LIST_RIGHT, ///< Right button.
+	WID_MTS_RIGHT_SCROLLBAR, ///< Scrollbar of right list.
 	WID_MTS_MUSICSET,   ///< Music set selection.
 	WID_MTS_ALL,        ///< All button.
 	WID_MTS_OLD,        ///< Old button.
