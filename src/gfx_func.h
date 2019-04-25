ef6378088f79301ddba1e3185d0a86875c3869ec
Sergii Pylypenko <x.pelya.x@gmail.com>
2019-04-25 19:08:16 +0300

    Increased video resolutions list size to 100

--- src/gfx_func.h
+++ src/gfx_func.h
@@ -71,7 +71,7 @@ extern DrawPixelInfo _screen;
 extern bool _screen_disable_anim;   ///< Disable palette animation (important for 32bpp-anim blitter during giant screenshot)
 
 extern int _num_resolutions;
-extern Dimension _resolutions[32];
+extern Dimension _resolutions[100];
 extern Dimension _cur_resolution;
 extern Palette _cur_palette; ///< Current palette
 
