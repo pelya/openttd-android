0cf9ecafa5a2a1faa1921979a0515fb2992455e7
pelya <x.pelya.x@gmail.com>
2020-03-20 00:15:51 +0200

    Fixed bad merge

--- src/script/script_config.hpp
+++ src/script/script_config.hpp
@@ -137,7 +137,7 @@ public:
 	/**
 	 * Randomize all settings the Script requested to be randomized.
 	 */
-	void AddRandomDeviation();
+	virtual void AddRandomDeviation();
 
 	/**
 	 * Is this config attached to an Script? In other words, is there a Script
