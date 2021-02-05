40f573cb9188937a73649c0164f123bf93dc231d
pelya <x.pelya.x@gmail.com>
2021-02-05 23:18:42 +0200

    Fixed Fontconfig not linking on Android

--- cmake/FindFontconfig.cmake
+++ cmake/FindFontconfig.cmake
@@ -32,28 +32,29 @@ This will define the following variables in your project:
 
 #]=======================================================================]
 
+if(NOT ANDROID)
+  # use pkg-config to get the directories and then use these values
+  # in the FIND_PATH() and FIND_LIBRARY() calls
+  find_package(PkgConfig QUIET)
+  pkg_check_modules(PKG_FONTCONFIG QUIET fontconfig)
+  set(Fontconfig_COMPILE_OPTIONS ${PKG_FONTCONFIG_CFLAGS_OTHER})
+  set(Fontconfig_VERSION ${PKG_FONTCONFIG_VERSION})
 
-# use pkg-config to get the directories and then use these values
-# in the FIND_PATH() and FIND_LIBRARY() calls
-find_package(PkgConfig QUIET)
-pkg_check_modules(PKG_FONTCONFIG QUIET fontconfig)
-set(Fontconfig_COMPILE_OPTIONS ${PKG_FONTCONFIG_CFLAGS_OTHER})
-set(Fontconfig_VERSION ${PKG_FONTCONFIG_VERSION})
+  find_path( Fontconfig_INCLUDE_DIR
+    NAMES
+      fontconfig/fontconfig.h
+    HINTS
+      ${PKG_FONTCONFIG_INCLUDE_DIRS}
+      /usr/X11/include
+  )
 
-find_path( Fontconfig_INCLUDE_DIR
-  NAMES
-    fontconfig/fontconfig.h
-  HINTS
-    ${PKG_FONTCONFIG_INCLUDE_DIRS}
-    /usr/X11/include
-)
-
-find_library( Fontconfig_LIBRARY
-  NAMES
-    fontconfig
-  PATHS
-    ${PKG_FONTCONFIG_LIBRARY_DIRS}
-)
+  find_library( Fontconfig_LIBRARY
+    NAMES
+      fontconfig
+    PATHS
+      ${PKG_FONTCONFIG_LIBRARY_DIRS}
+  )
+endif (NOT ANDROID)
 
 if(Fontconfig_INCLUDE_DIR AND NOT Fontconfig_VERSION)
   file(STRINGS ${Fontconfig_INCLUDE_DIR}/fontconfig/fontconfig.h _contents REGEX "^#define[ \t]+FC_[A-Z]+[ \t]+[0-9]+$")
