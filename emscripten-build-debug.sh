7c185bc75e2c223024f66b99d28bf6f4a113082c
Sergii Pylypenko <x.pelya.x@gmail.com>
2021-04-30 23:58:27 +0300

    Added Emscripten build scripts

---
+++ emscripten-build-debug.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+[ -z "`which emsdk`" ] && export PATH=`pwd`/../emsdk:$PATH
+
+[ -z "`which emsdk`" ] && { echo "Put emsdk into your PATH"; exit 1 ; }
+
+[ -z "$PATH_EMSDK" ] && PATH_EMSDK="`which emsdk | xargs dirname`"
+
+source "$PATH_EMSDK/emsdk_env.sh"
+
+mkdir -p build-wasm-debug
+cd build-wasm-debug
+
+[ -e build-host ] || {
+	rm -rf build-host
+	mkdir -p build-host
+	cd build-host
+	cmake ../.. -DOPTION_TOOLS_ONLY=ON || exit 1
+	make -j8 tools || exit 1
+	cd ..
+}
+
+embuilder build liblzma
+
+[ -e Makefile ] || emcmake cmake .. -DHOST_BINARY_DIR=$(pwd)/build-host -DCMAKE_BUILD_TYPE=Debug -DOPTION_USE_ASSERTS=OFF || exit 1
+emmake make -j8 VERBOSE=1 || exit 1
+
+cp -f *.html *.js *.mem *.data *.wasm ../media/openttd.256.png ../os/emscripten/openttd.webapp /var/www/html/
